64a313082cc51b332f809e5dd392d76f0bb21ad3
Marcin Kujawa <biz.marcin@gmail.com>
2017-07-24 16:52:56 +0200

    part of code

---
+++ phdServiceForPSP/Service1.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace phdServiceForPSP
+{
+    partial class Service1
+    {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            // 
+            // Service1
+            // 
+            this.ServiceName = "Service1";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.EventLog eventLog1;
+    }
+}
---
+++ phdServiceForPSP/Service1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phdServiceForPSP
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+            if (!System.Diagnostics.EventLog.SourceExists("pspService"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource("pspService", "PSP log");
+            }
+            eventLog1.Source = "testowaUslugaMarcin";
+            eventLog1.Log = "PSP log";
+
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("Service was started");
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("Service was stoped");
+        }
+
+        protected override void OnContinue()
+        {
+            eventLog1.WriteEntry("Service is running");
+        }
+    }
+}
